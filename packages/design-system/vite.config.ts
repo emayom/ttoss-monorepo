717929086b6ec2acd91c711ef29643abe246882c
emayom <emayom7@gmail.com>
2024-01-08 09:39:44 +0900

    :wrench: Chore: svgr 플러그인 추가

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -2,13 +2,20 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify("v1.0.0"),
   },
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    svgr({
+      include: ["**/*.svg?react", resolve(__dirname, "src/**/*.svg?react")],
+    }),
+    react(),
+    vanillaExtractPlugin(),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: resolve(__dirname, "src") },
